f689ceaf60d43f7766992733ec30c113110b2cfa
jsanteler <juliansanteler@tsn.at>
2023-09-27 10:28:46 +0200

    neuesfeature

--- GitTest.java
+++ GitTest.java
@@ -1,5 +1,8 @@
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hallo Julian, erstelle deinen Branch und mach noch eine weitere Zeile ");
+
+
+        System.out.println("neuesFeaturevon Julian");
     }
 }
